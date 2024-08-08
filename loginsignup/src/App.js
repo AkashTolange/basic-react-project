4a8a4b292fa016af58eb5217cd3144f1cbe96751
Akash Tolange <akashhtolangee77@gmail.com>
2024-08-08 18:37:35 +0545

    deploying proj one

---
+++ loginsignup/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Loginsignup } from './Components/Loginsignup/Loginsignup';
+
+function App() {
+  return (
+        <Loginsignup/>
+  );
+}
+
+export default App;
